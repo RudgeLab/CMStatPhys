2c5295d4f063e6668bee1b3683ac009c2c1e38e6
Ignacio <Medina@MacBook-Pro-de-Ignacio.local>
2018-09-09 13:28:44 -0300

    fixed repetition bug

--- Convergent_Curves.py
+++ Convergent_Curves.py
@@ -24,13 +24,13 @@ path_to_write = root+"/convergent_curves"
 if not os.path.isdir(path_to_write):
     os.makedirs(path_to_write)
 for simulation in datafiles:
-    print 'Loading and running '+ datafolders[i]
-    cellstates = App.add_protein_pickles(simulation,startframe,nframes,lambd = lambd)
-    cellstates_reordered = [cellstates[t]['cellStates'] for t in range(nframes)]
-    
-    cellstate = App.add_radius(cellstates_reordered) #get r for each cell
-    
     for repet in range(1,repetitions+1):
+        print 'Loading and running '+ datafolders[i]
+        cellstates = App.add_protein_pickles(simulation,startframe,nframes,lambd = lambd)
+        cellstates_reordered = [cellstates[t]['cellStates'] for t in range(nframes)]
+    
+        cellstate = App.add_radius(cellstates_reordered) #get r for each cell
+    
         n_norm, r_bins = PR.obtain_convergent_curves(cellstate,t1,t2,nbins)
     
         np.savetxt(path_to_write+'/'+folders[i]+'-r-'+str(repet)+'.gz',n_norm)
