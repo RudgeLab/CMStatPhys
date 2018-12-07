67828d4bf63de812efbf0dd9fda22d7cd41e5e28
Ignacio <Medina@MacBook-Pro-de-Ignacio.local>
2018-12-06 23:15:53 -0300

    Norm added

--- Tree_Analysis.py
+++ Tree_Analysis.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import numpy as np
 import cPickle
 import matplotlib.pyplot as plt
@@ -12,8 +13,9 @@ datadir = root+"/data"
        
     
 datafolders,datafiles,folders = GetSubDir(datadir)
-    
-t1 = 300
+print sys.argv[1]
+t1 = int(sys.argv[1])
+
 t2 = 500
 t_tree = 500
 #t2 = 1700 
@@ -21,15 +23,19 @@ t_tree = 500
 i = 0
 Gamma_hist = []
 Nb_hist = []
+Nnorm_hist = []
 path_to_write = root+"/histograms"
 pw1 = path_to_write+"/Gamma"
 pw2 =  path_to_write+"/Nbranch"
+pw3 = path_to_write+"/Nnorm"
 if not os.path.isdir(path_to_write):
     os.makedirs(path_to_write)
 if not os.path.isdir(pw1):
     os.makedirs(pw1)
 if not os.path.isdir(pw2):
     os.makedirs(pw2)
+if not os.path.isdir(pw3):
+    os.makedirs(pw3)
 
 for simulation in datafiles:
     Tree = cPickle.load(open(datafolders[i]+"/Tree_"+str(t_tree)+".pickle","rb"))
@@ -47,12 +53,15 @@ for simulation in datafiles:
         N_b = 0.0
         branch_cells = [cellstate_f[id] for id in Tree.branch[bid].nodes if cellstate_f.has_key(id)]
         N_b = float(len(branch_cells))
+        N_norm = N_b/N_T
         Gamma = (N_b/N_T) / expected
         if Gamma != 0:
             Gamma_hist.append(Gamma)
             Nb_hist.append(N_b)
+            Nnorm_hist.append(N_norm)
         else:
             print simulation, bid
     i += 1
 np.savetxt(pw1+"/Gamma-"+str(t1)+"-"+str(t2)+"-"+str(t_tree)+'.gz',Gamma_hist)
 np.savetxt(pw2+"/Nb-"+str(t1)+"-"+str(t2)+"-"+str(t_tree)+'.gz',Nb_hist)
+np.savetxt(pw3+"/Nnorm-"+str(t1)+"-"+str(t2)+"-"+str(t_tree)+'.gz',Nnorm_hist)
